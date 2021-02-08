9aa1a933cb286466b9eceac800ace17989433ac4
andrew <andrive@siue.edu>
2021-02-08 16:12:31 -0600

    added mixed design index.html

--- PlasticCracks/settings.py
+++ PlasticCracks/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pccalc.apps.PccalcConfig',
     'tcpowers.apps.TcpowersConfig',
+    'mixdesign.apps.MixdesignConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mixdesign/views.py
+++ mixdesign/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'mixdesign/index.html')
+    return HttpResponse("Hello, world. You're at the polls index.")
 
