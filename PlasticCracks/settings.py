7befeb334faf20594b6bfc3c3961d11ceb3b594b
Joshua Chastain <jochast@siue.edu>
2021-02-08 17:25:30 -0600

    Added template

--- PlasticCracks/settings.py
+++ PlasticCracks/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'pccalc.apps.PccalcConfig',
     'tcpowers.apps.TcpowersConfig',
     'mixdesign.apps.MixdesignConfig',
+    'bogue.apps.BogueConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
