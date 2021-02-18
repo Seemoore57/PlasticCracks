508acd541d4dea6f9a660ef2252443912caf8735
naloepk <nateloepker@gmail.com>
2021-02-17 21:26:17 -0600

    workin on forms

---
+++ tcpowers/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class TCPowersCalc(forms.Form):
+    WCRatio = forms.FloatField(min_value=0, max_value=100)
+    DegOfHydra = forms.FloatField(min_value=0, max_value=100)
