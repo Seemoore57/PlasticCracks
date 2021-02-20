78fa72213deca12a65ff9884ca6a7622146cd969
Joshua Chastain <jochast@siue.edu>
2021-02-19 23:20:29 -0600

    Fixed compare with sessions + created compare2.html + started on chart tooltips

--- bogue/forms.py
+++ bogue/forms.py
@@ -6,12 +6,3 @@ class BogueCalculator(forms.Form):
     alumina = forms.FloatField(min_value=0, max_value=100)
     rust = forms.FloatField(min_value=0, max_value=100)
 
-class CompareForm(forms.Form):
-    lime = forms.FloatField(min_value=0, max_value=100)
-    silica = forms.FloatField(min_value=0, max_value=100)
-    alumina = forms.FloatField(min_value=0, max_value=100)
-    rust = forms.FloatField(min_value=0, max_value=100)
-    oldLime = forms.FloatField(min_value=0, max_value=100)
-    oldSilica = forms.FloatField(min_value=0, max_value=100)
-    oldAlumina = forms.FloatField(min_value=0, max_value=100)
-    oldRust = forms.FloatField(min_value=0, max_value=100)
